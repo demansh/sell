657125d6589c49171dd5c0b986fc1241fd74507a
aleksey.d <aleksey.d@miro.com>
2026-02-16 08:57:29 +0400

    post

---
+++ assets/js/post.js
@@ -0,0 +1,29 @@
+function moveCarousel(direction) {
+  const carousel = document.getElementById('carousel');
+  const scrollAmount = carousel.offsetWidth;
+  carousel.scrollBy({
+    left: scrollAmount * direction,
+    behavior: 'smooth'
+  });
+}
+
+// Опционально: перетаскивание мышкой (drag-to-scroll)
+const slider = document.getElementById('carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => isDown = false);
+slider.addEventListener('mouseup', () => isDown = false);
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
